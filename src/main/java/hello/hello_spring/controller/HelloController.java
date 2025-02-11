706d022a139c2fe360cfa62079b9d83bc3cff0ea
choihongjun <vvclsbalam@gmail.com>
2025-02-12 00:58:10 +0900

    feat: helloMvc 메소드 구현

--- src/main/java/hello/hello_spring/controller/HelloController.java
+++ src/main/java/hello/hello_spring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hello_spring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -10,7 +11,13 @@ public class HelloController {
     // 웹에서 /hello라고 들어오면 호출
     @GetMapping("hello")
     public String hello(Model model) {
-        model.addAttribute("data", "spring!!"); // 키가 data고 값이 hello!!
+        model.addAttribute("data", "hello!!"); // 키가 data고 값이 hello!!
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name); // 키와 값
+        return "hello-template"; // hello-template.html 파일 실행
     }
 }
