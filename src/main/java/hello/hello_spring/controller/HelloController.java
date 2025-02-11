3efcf4d6cd638bad699ef769085db763e93d722c
choihongjun <vvclsbalam@gmail.com>
2025-02-12 01:44:52 +0900

    feat: helloString 메소드 구현

--- src/main/java/hello/hello_spring/controller/HelloController.java
+++ src/main/java/hello/hello_spring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -19,5 +20,11 @@ public class HelloController {
     public String helloMvc(@RequestParam("name") String name, Model model) {
         model.addAttribute("name", name); // 키와 값
         return "hello-template"; // hello-template.html 파일 처리
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody // http프로토콜 body에 return
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
     }
 }
