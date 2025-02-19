120d58dcd9f0ca4a697dfe8dfca85a33c020a0f5
choihongjun <vvclsbalam@gmail.com>
2025-02-20 00:51:01 +0900

    feat: join, validateDuplicateMember 메소드 구현

--- src/main/java/hello/hello_spring/service/MemberService.java
+++ src/main/java/hello/hello_spring/service/MemberService.java
@@ -1,4 +1,33 @@
 package hello.hello_spring.service;
 
+import hello.hello_spring.domain.Member;
+import hello.hello_spring.repository.MemberRepository;
+import hello.hello_spring.repository.MemoryMemberRepository;
+
+import java.util.Optional;
+
 public class MemberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원 가입
+     */
+    public long join(Member member) {
+        validateDuplicateMember(member); // 중복 회원 검증
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    // 중복 회원 검증
+    private void validateDuplicateMember(Member member) {
+//        Optional<Member> result = memberRepository.findByName(member.getName());
+//        result.ifPresent(m -> {
+//            throw new IllegalStateException("이미 존재하는 회원입니다.");
+//        });
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
 }
