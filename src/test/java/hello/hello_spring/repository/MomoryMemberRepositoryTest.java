8a92647788af7b238538d5c1074f6f67ef8b72cd
choihongjun <vvclsbalam@gmail.com>
2025-02-20 00:25:02 +0900

    feat: findAll 메소드 테스트

--- src/test/java/hello/hello_spring/repository/MomoryMemberRepositoryTest.java
+++ src/test/java/hello/hello_spring/repository/MomoryMemberRepositoryTest.java
@@ -4,6 +4,7 @@ import hello.hello_spring.domain.Member;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
@@ -36,5 +37,26 @@ class MomoryMemberRepositoryTest {
 
         Member result = repository.findByName("spring1").get();
         assertThat(result).isEqualTo(member1);
+
+        // 오류
+//        Member result = repository.findByName("spring2").get();
+//        assertThat(result).isEqualTo(member1);
+    }
+
+    @Test
+    public void findByAll() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        List<Member> result = repository.findAll();
+        assertThat(result.size()).isEqualTo(2);
+
+        // 오류
+//        assertThat(result.size()).isEqualTo(3);
     }
 }
