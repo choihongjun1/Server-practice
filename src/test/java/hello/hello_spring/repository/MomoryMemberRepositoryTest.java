6530e0539b0dec8429947496ea6e51a0f2ff805e
choihongjun <vvclsbalam@gmail.com>
2025-02-20 00:19:27 +0900

    feat: findByName 메소드 테스트

--- src/test/java/hello/hello_spring/repository/MomoryMemberRepositoryTest.java
+++ src/test/java/hello/hello_spring/repository/MomoryMemberRepositoryTest.java
@@ -4,6 +4,8 @@ import hello.hello_spring.domain.Member;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.*;
 
 class MomoryMemberRepositoryTest {
@@ -20,5 +22,19 @@ class MomoryMemberRepositoryTest {
 //        Assertions.assertEquals(member, null);
         assertThat(member).isEqualTo(result);
 //        assertThat(member).isEqualTo(null);
+    }
+
+    @Test
+    public void findByName() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        Member result = repository.findByName("spring1").get();
+        assertThat(result).isEqualTo(member1);
     }
 }
