040e6e13a887ef977a0c4fc2c81d026fc4aac81c
choihongjun <vvclsbalam@gmail.com>
2025-02-20 00:13:58 +0900

    feat: save 메소드 테스트

--- src/test/java/hello/hello_spring/repository/MomoryMemberRepositoryTest.java
+++ src/test/java/hello/hello_spring/repository/MomoryMemberRepositoryTest.java
@@ -1,4 +1,24 @@
 package hello.hello_spring.repository;
 
-public class MomoryMemberRepositoryTest {
+import hello.hello_spring.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class MomoryMemberRepositoryTest {
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @Test
+    public void save() {
+        Member member = new Member();
+        member.setName("spring");
+        repository.save(member);
+        Member result = repository.findById(member.getId()).get();
+//        System.out.println("result: " + (result == member));
+//        Assertions.assertEquals(member, result);
+//        Assertions.assertEquals(member, null);
+        assertThat(member).isEqualTo(result);
+//        assertThat(member).isEqualTo(null);
+    }
 }
