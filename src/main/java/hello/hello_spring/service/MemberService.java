0ae5bc802f1ddbd12e6d874c6bd0a993bb7b3d46
choihongjun <vvclsbalam@gmail.com>
2025-02-24 06:50:38 +0900

    feat: join 시간 계산 구현

--- src/main/java/hello/hello_spring/service/MemberService.java
+++ src/main/java/hello/hello_spring/service/MemberService.java
@@ -22,10 +22,17 @@ public class MemberService {
      * 회원 가입
      */
     public long join(Member member) {
-        validateDuplicateMember(member); // 중복 회원 검증
+        long start = System.currentTimeMillis();
 
-        memberRepository.save(member);
-        return member.getId();
+        try {
+            validateDuplicateMember(member); // 중복 회원 검증
+            memberRepository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join = " + timeMs + "ms");
+        }
     }
 
     // 중복 회원 검증
