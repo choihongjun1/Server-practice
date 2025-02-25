e3f71afbc25b0c396c51b7d0e40d1f3b4b5fce59
choihongjun <vvclsbalam@gmail.com>
2025-02-26 02:56:34 +0900

    feat: 시간 측정 로직 삭제

--- src/main/java/hello/hello_spring/service/MemberService.java
+++ src/main/java/hello/hello_spring/service/MemberService.java
@@ -22,17 +22,9 @@ public class MemberService {
      * 회원 가입
      */
     public long join(Member member) {
-        long start = System.currentTimeMillis();
-
-        try {
-            validateDuplicateMember(member); // 중복 회원 검증
-            memberRepository.save(member);
-            return member.getId();
-        } finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish - start;
-            System.out.println("join = " + timeMs + "ms");
-        }
+        validateDuplicateMember(member); // 중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
     }
 
     // 중복 회원 검증
@@ -51,14 +43,7 @@ public class MemberService {
      * 전체 회원 조회
      */
     public List<Member> findMembers() {
-        long start = System.currentTimeMillis();
-        try {
             return memberRepository.findAll();
-        } finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish - start;
-            System.out.println("findMembers = " + timeMs + "ms");
-        }
     }
 
     public Optional<Member> findOne(long memberId) {
